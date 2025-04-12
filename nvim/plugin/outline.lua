ac83b25e19fba892847ad0db255bb08f741e79ec
Scott Hansen <tech@firecat53.net>
2025-04-11 18:13:45 -0700

    Auto-close outline window

--- nvim/plugin/outline.lua
+++ nvim/plugin/outline.lua
@@ -3,5 +3,9 @@ if vim.g.did_load_outline_plugin then
 end
 vim.g.did_load_outline_plugin = true
 
-require('outline').setup {}
+require('outline').setup({
+  outline_window = {
+    auto_close = true,
+  },
+})
 vim.keymap.set('n', '<leader>o', vim.cmd.Outline, { desc = 'Toggle Outline' })
